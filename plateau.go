a644905b7c7d0a4f0ba3a2bc3c0d6d8bd6cbe95f
TitouanPrl <titouan.pradal@gmail.com>
2023-11-16 15:00:43 +0100

    first commit

--- plateau.go
+++ plateau.go
@@ -1,5 +1,21 @@
 package main
 
-func pickANumberInATab(tab []int) (int, error) {
+import (
+	"errors"
+	"math/rand"
+)
 
+func pickANumberInATab(tab []int) (int, error) {
+	if tab == nil {
+		return 0, errors.New("there is no tab in")
+	}
+
+	/* Getting the number still available */
+	tabLenght := len(tab)
+	if tabLenght == 0 {
+		return 0, errors.New("tab empty")
+	}
+
+	/* Choosing a random num between those still available */
+	res := rand.Intn(tabLenght)
 }
