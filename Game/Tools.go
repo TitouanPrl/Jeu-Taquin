66580b09f0fbbff0966d6673a7594ee3f3234a89
TitouanPrl <titouan.pradal@gmail.com>
2023-11-17 10:25:04 +0100

    checkWinCondition

--- Game/Tools.go
+++ Game/Tools.go
@@ -2,6 +2,7 @@ package Game
 
 import (
 	"errors"
+	"fmt"
 )
 
 /* abs returns the absolute value of x */
@@ -12,8 +13,8 @@ func abs(x int) int {
 	return x
 }
 
-/* moveItem swap an item with the null item if they're neighbours */
-func moveItem(tab [3][3]int, item int) ([3][3]int, error) {
+/* MoveItem swap an item with the null item if they're neighbours */
+func MoveItem(tab [3][3]int, item int) ([3][3]int, error) {
 	if item <= 0 {
 		return [3][3]int{}, errors.New("impossible de trouver une case négative")
 	}
@@ -54,4 +55,28 @@ func moveItem(tab [3][3]int, item int) ([3][3]int, error) {
 	tab[zeroY][zeroX] = item
 
 	return tab, nil
+}
+
+func checkWinCondition(tab [3][3]int, nbCoup int) (bool, error) {
+	if tab == [3][3]int{} {
+		return false, errors.New("le tableau est vide")
+	}
+
+	lenght := 3
+	count := 0
+	win := true
+
+	/* Checking if the slice is sorted, which would mean the game is won */
+	for i := 0; i < lenght; i++ {
+		for j := 0; j < lenght; j++ {
+			if tab[i][j] != count {
+				win = false
+			}
+			count++
+		}
+	}
+
+	fmt.Println("Victoire ! Félicitiations vous avez réussi à triompher du Taquin en :", nbCoup)
+
+	return win, nil
 }
