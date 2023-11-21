92fe4284349071971943fa9ffbe330eb01396389
TitouanPrl <titouan.pradal@gmail.com>
2023-11-21 11:11:53 +0100

    path

--- Game/AI_Method.go
+++ Game/AI_Method.go
@@ -1,5 +1,16 @@
 package Game
 
+import "container/list"
+
+type node struct {
+	cost   int
+	rank   int
+	parent *node
+	open   bool
+	closed bool
+	index  int
+}
+
 // func astar(tuile int) list.List {
 /* Définition des variables */
 //	fileAvoir := list.New()
@@ -10,4 +21,17 @@ package Game
 
 // }
 
-// func chemin(E)
+/* path returns a list of the nodes between F and E */
+func path(E node, F node) list.List {
+	listRes := list.New()
+
+	listRes.PushFront(F)
+
+	/* Checks if E is the parent of F, otherwise adds the parent of F to the list */
+	for check := true; check; check = *F.parent != E {
+		listRes.PushFront(F.parent)
+		F = *F.parent
+	}
+
+	return *listRes
+}
